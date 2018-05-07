5334ef8b9b9677b9bb3334accb46c3fa3b55d3a6
Kirk Kaiser <kirk@zothcorp.com>
2018-05-07 13:33:28 -0400

    Get rid of old image joiner before creating cron job

--- joinImages.py
+++
@@ -1,8 +0,0 @@
-import glob
-import shutil
-
-# this joins each of the individual detections into one long image sequence
-for filename in glob.glob('bird*'):
-    print(filename)
-    for i in range(240):
-        shutil.copyfile(filename + ('/%05d.jpg' % i), ('outvideo/%05d.jpg'  % ((int(filename[4:]) - 1) * 240 + i)))
